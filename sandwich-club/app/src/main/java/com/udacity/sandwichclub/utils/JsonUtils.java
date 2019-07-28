64507ff2af8e52978755683a8458f58e9c891590
Ashraf Sarhan <ashraf.sar7an@gmail.com>
2019-07-28 22:19:55 +0200

    Define constant strings as separate variables

--- sandwich-club/app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ sandwich-club/app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -12,17 +12,24 @@ import java.util.List;
 public class JsonUtils {
 
     private static final String TAG = "JsonUtils";
+    public static final String NAME = "name";
+    public static final String MAIN_NAME = "mainName";
+    public static final String ALSO_KNOWN_AS = "alsoKnownAs";
+    public static final String PLACE_OF_ORIGIN = "placeOfOrigin";
+    public static final String DESCRIPTION = "description";
+    public static final String IMAGE = "image";
+    public static final String INGREDIENTS = "ingredients";
 
     public static Sandwich parseSandwichJson(String json) {
         try {
             Sandwich sandwich = new Sandwich();
             JSONObject sandwichJson = new JSONObject(json);
-            sandwich.setMainName(sandwichJson.getJSONObject("name").getString("mainName"));
-            sandwich.setAlsoKnownAs(toStringList(sandwichJson.getJSONObject("name").getJSONArray("alsoKnownAs")));
-            sandwich.setPlaceOfOrigin(sandwichJson.getString("placeOfOrigin"));
-            sandwich.setDescription(sandwichJson.getString("description"));
-            sandwich.setImage(sandwichJson.getString("image"));
-            sandwich.setIngredients(toStringList(sandwichJson.getJSONArray("ingredients")));
+            sandwich.setMainName(sandwichJson.getJSONObject(NAME).getString(MAIN_NAME));
+            sandwich.setAlsoKnownAs(toStringList(sandwichJson.getJSONObject(NAME).getJSONArray(ALSO_KNOWN_AS)));
+            sandwich.setPlaceOfOrigin(sandwichJson.getString(PLACE_OF_ORIGIN));
+            sandwich.setDescription(sandwichJson.getString(DESCRIPTION));
+            sandwich.setImage(sandwichJson.getString(IMAGE));
+            sandwich.setIngredients(toStringList(sandwichJson.getJSONArray(INGREDIENTS)));
             Log.d(TAG, "Sandwich object: ".concat(sandwich.toString()));
             return sandwich;
         } catch (JSONException e) {
