b09cb1d765d1db561962657d463ea4f7aee70925
Ashraf Sarhan <ashraf.sar7an@gmail.com>
2019-11-04 11:44:39 +0100

    Update unit test

--- build-it-bigger/app/src/androidTest/java/com/udacity/gradle/builditbigger/EndPointAsyncTaskUnitTest.java
+++ build-it-bigger/app/src/androidTest/java/com/udacity/gradle/builditbigger/EndPointAsyncTaskUnitTest.java
@@ -3,26 +3,35 @@ package com.udacity.gradle.builditbigger;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 import android.text.TextUtils;
+import org.junit.Test;
 
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
 
 public class EndPointAsyncTaskUnitTest extends ApplicationTestCase<Application> {
+
+    private String joke;
 
     public EndPointAsyncTaskUnitTest() {
         super(Application.class);
     }
 
+    @Test
     public void testGetJokeTask() throws ExecutionException, InterruptedException {
-        EndPointAsyncTask testJoke = new EndPointAsyncTask(new EndPointAsyncTask.Callback() {
+        EndPointAsyncTask endPointAsyncTask = new EndPointAsyncTask(new EndPointAsyncTask.Callback() {
             @Override
             public void onFinished(String result) {
 
             }
         });
-        testJoke.execute();
-        String joke = testJoke.get();
-        assertNotNull(joke);
-        assertFalse(TextUtils.isEmpty(joke));
+        endPointAsyncTask.execute();
+        try {
+            joke = endPointAsyncTask.get(10, TimeUnit.SECONDS);
+            assertNotNull("Joke does not fetched successfully", joke);
+            assertFalse("Joke does not fetched successfully", joke.isEmpty());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
 
